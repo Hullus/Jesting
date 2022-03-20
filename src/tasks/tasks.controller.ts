b11e780733fb42db35ffa1d36fbd6854c4d53a02
Hullus <73313334+Hullus@users.noreply.github.com>
2022-03-20 23:03:11 +0400

    Tasks module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { Task } from "./tasks.model";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { stat } from "fs";
 
-@Controller('tasks')
+@Controller("tasks")
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {
+  }
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(":id")
+  getOneTask(@Param("id") id: string): Task {
+    return this.tasksService.getOneTask(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch("/:id/status")
+  updateTask(
+    @Param("id") id: string,
+    @Body("status") status: string
+  ): Task {
+    console.log("rame rume");
+    return this.tasksService.updateTask(id, status);
+  }
+
+  @Delete(":id")
+  deleteTask(@Param("id") id: string): void {
+    this.tasksService.deleteTask(id);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Task, taskStatus } from "./tasks.model";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getOneTask(id): Task {
+    return this.tasks.find(item => item.id === id);
+  }
+
+  createTask(createTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: taskStatus.OPEN
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTask(id, status): Task{
+    const task = this.tasks.find(item => item.id === id);
+    task.status = status;
+    // if ()
+    return task;
+  }
+
+  deleteTask(id): void {
+    const index = this.tasks.findIndex(item => item.id === id);
+    this.tasks.splice(index, 1);
+  }
+}
+
